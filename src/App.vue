3f68c71425f8bcbb4acc3b04bb59d5383660297a
Dan Høegh <dan.hoegh@gmail.com>
2021-10-15 15:05:46 +0200

    kljadxlkjkljæhfds

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,66 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <div>
+      <input v-model="value" type="text" />
+    </div>
+    <!-- modifiers: v-model.modifier.modifier -->
+    <div>
+      <!-- <input v-model.lazy="value" type="text" /> -->
+      <!-- this waits for a loss of focus before updating. Performance increased! -->
+    </div>
+    <div>
+      <!-- <input v-model.number="value" type="text" /> -->
+      <!-- will ignore everything after it was last able to parse using parse.float -->
+    </div>
+    <div>
+      <!-- <input v-model.trim="value" type="text" /> -->
+      <!-- removes any spaces at the beginning and end of the string -->
+    </div>
+    <div>
+      {{ value }}
+    </div>
+    <div>
+      <button @click="updateText">Reset inputs</button>
+    </div>
+    <div>
+      <button @click="setShowTrue">Set show = true</button>
+    </div>
+    <div>
+      <button @click="setShowFalse">Set show = false</button>
+    </div>
+    <div>
+      <button @click="toggleShow">Toggle show</button>
+    </div>
+
+    <div>show is: {{ show }}</div>
+    <div v-if="show">Show this IF show is TRUE</div>
+    <div v-else>Show this IF show is FALSE</div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data: () => ({
+    value: "",
+    show: false,
+  }),
+  methods: {
+    updateText() {
+      this.value = "Test";
+    },
+    setShowTrue() {
+      this.show = true;
+    },
+    setShowFalse() {
+      this.show = false;
+    },
+    toggleShow() {
+      this.show = !this.show;
+    },
+  },
+  components: {},
+};
 </script>
 
 <style>
@@ -22,5 +71,10 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  text-align: left;
+}
+
+div {
+  margin: 20px;
 }
 </style>
